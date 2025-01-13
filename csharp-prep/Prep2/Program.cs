84d3a2ad0fd080e92905542a28b1ec3aa392cfb3
etvance <etvance@byui.edu>
2025-01-13 13:00:48 -0700

    Grading Program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,83 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?: ");
+        string gradePrecentText = Console.ReadLine();
+        int gradePercent = int.Parse(gradePrecentText);
+        Math.DivRem(gradePercent, 10, out int remainder);
+
+        System.Console.WriteLine($"{remainder}");
+
+        string gradeLetter = "";
+
+        if (gradePercent > 90)
+        {
+            if (remainder >= 3)
+            {
+                gradeLetter = "A";
+            }
+            else if (remainder < 3)
+            {
+                gradeLetter = "A-";
+            }
+        }
+        else if (gradePercent >= 80)
+        {
+            if (remainder >= 7)
+            {
+                gradeLetter = "B+";
+            }
+            else if (remainder >= 3)
+            {
+                gradeLetter = "B";
+            }
+            else if (remainder < 3)
+            {
+                gradeLetter = "B-";
+            }
+        }
+        else if (gradePercent >= 70)
+        {
+            if (remainder >= 7)
+            {
+                gradeLetter = "C+";
+            }
+            else if (remainder >= 3)
+            {
+                gradeLetter = "C";
+            }
+            else if (remainder < 3)
+            {
+                gradeLetter = "C-";
+            }        }
+        else if (gradePercent >= 60)
+        {
+            if (remainder >= 7)
+            {
+                gradeLetter = "D+";
+            }
+            else if (remainder >= 3)
+            {
+                gradeLetter = "D";
+            }
+            else if (remainder < 3)
+            {
+                gradeLetter = "D-";
+            }
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        System.Console.WriteLine($"Your letter grade is {gradeLetter}.");
+        if (gradePercent >= 70)
+        {
+            System.Console.WriteLine("Congradulations on passing the course.");
+        }
+        else
+        {
+            System.Console.WriteLine("Due to your Grade you did not pass. Try better next time!");
+        }
     }
 }
