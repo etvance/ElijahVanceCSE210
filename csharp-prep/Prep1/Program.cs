49c55ac650f75324b5403125a8ae70acf526e664
etvance <etvance@byui.edu>
2025-01-13 12:19:50 -0700

    Vance, Elijah Vance

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your fist Name?: ");
+        string Name1 = Console.ReadLine();
+
+        Console.Write("What is your last Name?: ");
+        string Name2 = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {Name2}, {Name1} {Name2}.");
     }
 }
