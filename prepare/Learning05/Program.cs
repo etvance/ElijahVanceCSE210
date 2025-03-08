da2252a2a755e7ea9f8fcdd2e629312db2044c8a
etvance <etvance@byui.edu>
2025-03-08 14:22:21 -0700

    prepare code  and classes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Purple", 4);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Yellow", 5, 3);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("White", 7);
+        shapes.Add(shape3);
+
+        foreach (Shape x in shapes)
+        {
+            string color = x.GetColor();
+            double area = x.GetArea();
+            System.Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
+
 }
